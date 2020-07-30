1c933d24f686a4723875b7dcf45ce3f2e50fe821
porigonop <alievre@laposte.net>
2020-07-30 11:27:01 +0200

    lexer

--- calculatrice.py
+++ calculatrice.py
@@ -58,7 +58,45 @@ class Calcul(Visitor):
     def print_result(self):
         print(self.memory)
 
-calcul_visitor = Calcul()
-op = Operation(Element(7), Element(3), Operator('+'))
-Operation(op, op, Operator('+')).accept(calcul_visitor)
-calcul_visitor.print_result()
+
+class Token:
+    operator = False
+    empty = False
+    def __init__(self, token):
+        self.token = token
+        if token == '+':
+            self.operator = True
+        elif token == '':
+            self.empty = True
+        else:
+            self.token = int(self.token)
+
+class Lexer:
+    def __init__(self, stream):
+        self.stream = stream
+
+    def get_token(self):
+        next_space = self.stream.find(' ')
+        if next_space == -1:
+            token = Token(self.stream)
+            self.stream = ''
+            return token
+        token = self.stream[:next_space]
+        self.stream = self.stream[next_space + 1:]
+        return Token(token)
+
+def test_ast():
+    calcul_visitor = Calcul()
+    op = Operation(Element(7), Element(3), Operator('+'))
+    Operation(op, op, Operator('+')).accept(calcul_visitor)
+    calcul_visitor.print_result()
+
+def test_lexer():
+    string = '1 + 3 + 4 + 5 + 1 + 0'
+    lexer = Lexer(string)
+    token = lexer.get_token()
+    while (not token.empty):
+        print(token.token)
+        token = lexer.get_token()
+
+test_lexer()
